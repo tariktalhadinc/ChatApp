a0dac4efc2d934142d2e5c44754b45bb841c5772
tariktalhadinc <tariktalhadinc@gmail.com>
2023-01-18 16:23:26 +0300

    test step commit
    app tested and working

---
+++ ChatApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs
+{
+    public class ChatHub : Hub 
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "User connected!");
+            await base.OnConnectedAsync();
+        }
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("GetThatMessageDude", user, message);
+        }
+    }
+}
